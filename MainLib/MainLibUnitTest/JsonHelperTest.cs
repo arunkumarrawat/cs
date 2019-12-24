5d3ae2bf2035191d7a6d95ac811e2e88e974d2c7
emacslisp <java.gdb@gmail.com>
2019-12-25 00:24:34 +1100

    #json - add json helper and unit test

---
+++ MainLib/MainLibUnitTest/JsonHelperTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Serialization;
+using MainLib;
+
+namespace MainLibUnitTest
+{
+    [DataContract]
+    class Person
+    {
+        [DataMember]
+        internal string name;
+
+        [DataMember]
+        internal int age;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class JsonHelperTest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void PersonToJson()
+        {
+            Person p = new Person();
+            p.name = "John";
+            p.age = 42;
+
+            JsonHelper jsonHelper = new JsonHelper();
+            string result = jsonHelper.ObjectToJson<Person>(p);
+            Console.WriteLine(result);
+        }
+    }
+}
