35f5eb1f6f50ab59e033652e610200ac32d330e4
emacslisp <java.gdb@gmail.com>
2020-04-25 23:44:12 +1000

    add CSFtp CLI project from Scratch

---
+++ MainLib/CSFtp-CLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSFtp_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CSFtp command line and how to handle file");
+
+        }
+    }
+}
