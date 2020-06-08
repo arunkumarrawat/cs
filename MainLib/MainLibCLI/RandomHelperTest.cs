c445143cfe78d9dd890d2c095ecbe6f8823cc071
emacslisp <java.gdb@gmail.com>
2020-06-08 22:37:03 +1000

    add RandomHelper

---
+++ MainLib/MainLibCLI/RandomHelperTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainLib;
+
+namespace MainLibCLI
+{
+    public class RandomHelperTest
+    {
+        public static void TMain(string[] args)
+        {
+            for(int i=0;i<100;i++)
+                Console.WriteLine(RandomHelper.GetRandomInt(1, 100+i));
+            
+        }
+    }
+}
