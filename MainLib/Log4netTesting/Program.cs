be21bebf19af52c80a1ac5be86b72a6dccd4e156
emacslisp <java.gdb@gmail.com>
2021-01-06 17:17:14 +1100

    move nhibernate and log4net example

---
+++ MainLib/Log4netTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4netTesting
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// @example: log4net - configuration and example
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            log.Info("This is a test", new Exception("test exception"));
+            log.Error("This is a test", new Exception("test exception"));
+        }
+    }
+}
