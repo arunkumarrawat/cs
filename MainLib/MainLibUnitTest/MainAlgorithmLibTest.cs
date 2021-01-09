13486bd054cb7a5c3ee10f2a58fdb453225a5181
emacslisp <java.gdb@gmail.com>
2021-01-10 08:56:34 +1100

    add C# min binary heap prioirty queue

---
+++ MainLib/MainLibUnitTest/MainAlgorithmLibTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MainLib.Algorithm;
+
+namespace MainLibUnitTest
+{
+    [TestClass]
+    public class MainAlgorithmLibTest
+    {
+        /// <summary>
+        /// Test Priority Queue Lib for C#
+        /// </summary>
+        [TestMethod]
+        public void PriorityQueueTest()
+        {
+            int[] a = { 9, 7, 3, 4, 6, 10, 20 };
+            PriorityQueue<int> pq = new PriorityQueue<int>();
+            foreach(int x in a)
+            {
+                pq.Enqueue(x);
+            }
+
+            Assert.AreEqual(pq.Peek(), 3);
+        }
+    }
+}
