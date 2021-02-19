cbeeed9bfc7e2d1773e1f4c4ffe4222179a51439
emacslisp <java.gdb@gmail.com>
2021-02-20 05:14:15 +1100

    1. add command line NKill 2. add winform project to copy file easily 3. add algorithm library

---
+++ MainLib/MainLib/SystemHelper.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace MainLib
+{
+    /// <summary>
+    /// Class to handler System
+    /// </summary>
+    public class SystemHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("shell32.dll", SetLastError = false)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsUserAnAdmin();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dwReserved"></param>
+        /// <param name="lpdwReserved"></param>
+        /// <returns></returns>
+        [DllImport("advpack.dll", SetLastError = false)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsNTAdmin(int dwReserved, int lpdwReserved);
+
+        /// <summary>
+        /// Is User An Admin
+        /// </summary>
+        /// <returns>true if User is Admin, otherwise false</returns>
+        public static bool IsUserAnAdminW()
+        {
+            return IsUserAnAdmin();
+        }
+
+        #region mouse click
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        /*
+         * How to Use mouse click
+         * SystemEvents.SessionEnding += new SessionEndingEventHandler(SystemEvents_SessionEnding);
+         */
+        #endregion
+        #region ShutdownWindows with high Privilege
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct TokPriv1Luid
+        {
+            public int Count;
+            public long Luid;
+            public int Attr;
+        }
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr
+        phtok);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        internal static extern bool LookupPrivilegeValue(string host, string name,
+        ref long pluid);
+
+        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall,
+        ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
+
+        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
+        internal static extern bool ExitWindowsEx(int flg, int rea);
+
+        internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
+        internal const int TOKEN_QUERY = 0x00000008;
+        internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        internal const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+        internal const int EWX_LOGOFF = 0x00000000;
+        internal const int EWX_SHUTDOWN = 0x00000001;
+        internal const int EWX_REBOOT = 0x00000002;
+        internal const int EWX_FORCE = 0x00000004;
+        internal const int EWX_POWEROFF = 0x00000008;
+        internal const int EWX_FORCEIFHUNG = 0x00000010;
+
+        /// <summary>
+        /// Do Exit Windows
+        /// </summary>
+        /// <param name="flg"></param>
+        public void DoExitWin(int flg)
+        {
+            bool ok;
+            TokPriv1Luid tp;
+            IntPtr hproc = GetCurrentProcess();
+            IntPtr htok = IntPtr.Zero;
+            ok = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+            tp.Count = 1;
+            tp.Luid = 0;
+            tp.Attr = SE_PRIVILEGE_ENABLED;
+            ok = LookupPrivilegeValue(null, SE_SHUTDOWN_NAME, ref tp.Luid);
+            ok = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+            ok = ExitWindowsEx(flg, 0);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputText"></param>
+        public void clipBoardSetText(string inputText)
+        {
+            Clipboard.SetText(inputText);
+        }
+        #endregion
+
+        #region Determine which .net framework version we install in computer
+        public void GetVersionFromRegistry()
+        {
+            // Opens the registry key for the .NET Framework entry.
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
+                // or later, you can use:
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (install == "") //no install info, must be later.
+                            Console.WriteLine(versionKeyName + "  " + name);
+                        else
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                Console.WriteLine(versionKeyName + "  " + name + "  SP" + sp);
+                            }
+
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "") //no install info, must be later.
+                                Console.WriteLine(versionKeyName + "  " + name);
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    Console.WriteLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    Console.WriteLine("  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        /// <summary>
+        /// kill progress By name
+        /// </summary>
+        /// <param name="progressName"></param>
+        public List<string> KillProcessByName(string progressName)
+        {
+            List<string> output = new List<string>();
+            Process[] p1 = Process.GetProcessesByName(progressName);
+            if (p1.Length == 0) return output;
+            for (int i = 0; i < p1.Length; i++)
+                if (p1[i] != null)
+                {
+                    bool result = PostMessage(p1[i].MainWindowHandle, 0x0012, 0, 0);// 0X12 is WM_QUIT
+                    if(result)
+                    {
+                        output.Add(p1[i].ProcessName + " exits successfully");
+                    }
+                }
+
+            return output;
+        }
+    }
+}
---
+++ MainLib/NKill/Program.cs
@@ -0,0 +1,30 @@
+﻿using MainLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if(args.Length != 1)
+                {
+
+                }
+                SystemHelper system = new SystemHelper();
+
+                system.KillProcessByName();
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
