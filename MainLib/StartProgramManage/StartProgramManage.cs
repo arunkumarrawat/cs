f6c343d3ce243aac4e0fe50aa93981bd68666095
emacslisp <java.gdb@gmail.com>
2021-02-24 10:35:00 +1100

    add windows start program manage

---
+++ MainLib/StartProgramManage/StartProgramManage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartProgramManage
+{
+    public partial class StartProgramManage : Form
+    {
+        public StartProgramManage()
+        {
+            InitializeComponent();
+        }
+    }
+}
