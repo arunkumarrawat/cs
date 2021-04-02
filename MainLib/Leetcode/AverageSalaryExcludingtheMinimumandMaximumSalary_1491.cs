4b36194bbb0b3c1c0448520afe469c044b8a425c
emacslisp <java.gdb@gmail.com>
2021-04-03 01:30:04 +1100

    finish leetcode 1491

---
+++ MainLib/Leetcode/AverageSalaryExcludingtheMinimumandMaximumSalary_1491.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class AverageSalaryExcludingtheMinimumandMaximumSalary_1491
+    {
+
+        public double Average(int[] salary)
+        {
+            double result = 0;
+            Array.Sort(salary);
+
+            for (int i = 1; i <= salary.Length - 2; i++)
+            {
+                result += salary[i];
+            }
+
+            return result / (salary.Length - 2);
+
+        }
+
+        public static void main()
+        {
+            int[] g = new int[] { 6000, 5000, 4000, 3000, 2000, 1000 };
+
+            AverageSalaryExcludingtheMinimumandMaximumSalary_1491 x = new AverageSalaryExcludingtheMinimumandMaximumSalary_1491();
+            Console.WriteLine(x.Average(g));
+
+        }
+    }
+}
--- MainLib/Leetcode/Program.cs
+++ MainLib/Leetcode/Program.cs
@@ -16,7 +16,7 @@ namespace Leetcode
 
             //ReorderList_143 r = new ReorderList_143();
             //r.main();
-            MinimumRemovetoMakeValidParentheses.main();
+            AverageSalaryExcludingtheMinimumandMaximumSalary_1491.main();
 
         }
     }
