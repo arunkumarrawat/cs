36943ce74dff1d0a35786acd63f4693b12eb0347
emacslisp <java.gdb@gmail.com>
2021-10-31 09:04:09 +1100

    add webscratch for 70-515

--- 70-505/Scratch/Scratch/Form1.Designer.cs
+++ 70-505/Scratch/Scratch/Form1.Designer.cs
@@ -28,20 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.groupBoxTabPage = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.flowLayoutPage = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel = new System.Windows.Forms.TabPage();
+            this.splitContainerPanel = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1.SuspendLayout();
+            this.groupBoxTabPage.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.flowLayoutPage.SuspendLayout();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.groupBoxTabPage);
+            this.tabControl1.Controls.Add(this.flowLayoutPage);
+            this.tabControl1.Controls.Add(this.tableLayoutPanel);
+            this.tabControl1.Controls.Add(this.splitContainerPanel);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(423, 316);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // groupBoxTabPage
+            // 
+            this.groupBoxTabPage.Controls.Add(this.groupBox1);
+            this.groupBoxTabPage.Location = new System.Drawing.Point(4, 22);
+            this.groupBoxTabPage.Name = "groupBoxTabPage";
+            this.groupBoxTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.groupBoxTabPage.Size = new System.Drawing.Size(415, 290);
+            this.groupBoxTabPage.TabIndex = 0;
+            this.groupBoxTabPage.Text = "GroupBox";
+            this.groupBoxTabPage.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 284);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(51, 98);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(51, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(51, 50);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPage
+            // 
+            this.flowLayoutPage.Controls.Add(this.flowLayoutPanel1);
+            this.flowLayoutPage.Location = new System.Drawing.Point(4, 22);
+            this.flowLayoutPage.Name = "flowLayoutPage";
+            this.flowLayoutPage.Padding = new System.Windows.Forms.Padding(3);
+            this.flowLayoutPage.Size = new System.Drawing.Size(415, 290);
+            this.flowLayoutPage.TabIndex = 1;
+            this.flowLayoutPage.Text = "FlowLayoutPanel";
+            this.flowLayoutPage.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.Controls.Add(this.tableLayoutPanel1);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(4, 22);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Size = new System.Drawing.Size(415, 290);
+            this.tableLayoutPanel.TabIndex = 2;
+            this.tableLayoutPanel.Text = "Table Layout Panel";
+            this.tableLayoutPanel.UseVisualStyleBackColor = true;
+            // 
+            // splitContainerPanel
+            // 
+            this.splitContainerPanel.Location = new System.Drawing.Point(4, 22);
+            this.splitContainerPanel.Name = "splitContainerPanel";
+            this.splitContainerPanel.Size = new System.Drawing.Size(415, 290);
+            this.splitContainerPanel.TabIndex = 3;
+            this.splitContainerPanel.Text = "Split Container Panel";
+            this.splitContainerPanel.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(415, 290);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(409, 284);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(423, 316);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.groupBoxTabPage.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.flowLayoutPage.ResumeLayout(false);
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage groupBoxTabPage;
+        private System.Windows.Forms.TabPage flowLayoutPage;
+        private System.Windows.Forms.TabPage tableLayoutPanel;
+        private System.Windows.Forms.TabPage splitContainerPanel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
---
+++ 70-515/WebScratch/WebScratch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebScratch.Global" Language="C#" %>
